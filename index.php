bb7037aaba2592c82209d8d6115c92eacb4d2561
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 11:56:38 +0200

    up exo3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo3</title>
+    </head>
+    <body>
+        <?php 
+            $km = 1;
+            echo "$km,";
+            $km = 3;
+            echo ($km . ',');
+            $km = 125;
+            echo ($km);
+        ?>
+    </body>
+</html>
